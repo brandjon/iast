0f70436cefb835289a0042d94328cc933b3c6436
Jon Brandvein <jon.brandvein@gmail.com>
2014-02-11 18:30:27 -0500

    Added pylang, with support for macros.

---
+++ iast/pylang.py
@@ -0,0 +1,120 @@
+"""Templating system for substituting ASTs and creating AST macros."""
+
+
+from iast.node import Expr, Call, Name, Load, Attribute
+from iast.pattern import PatVar, PatternTransformer
+
+
+__all__ = [
+    'extract_mod',
+    'MacroProcessor',
+]
+
+
+def extract_mod(tree, mode=None):
+    """Process a tree to extract a subtree of the top-level
+    Module node. Mode is one of the following strings:
+    
+        - mod:     the tree is returned unchanged (default)
+        - code:    the Module's list of statements is returned
+        - stmt_or_blank:  either a single statement or None is returned
+        - stmt:    a single statement is returned. The Module must have
+                       exactly one top-level statement.
+        - expr:    an expression is returned. The Module must have
+                       exactly one top-level statement, that is an
+                       Expr node.
+    """
+    if mode == 'mod':
+        pass
+    
+    elif mode == 'code':
+        tree = tree.body
+    
+    elif mode == 'stmt_or_blank':
+        if len(tree.body) == 0:
+            return None
+        elif len(tree.body) == 1:
+            tree = tree.body[0]
+        else:
+            raise ValueError('Mode "{}" requires zero or one statements '
+                             '(got {})'.format(mode, len(tree.body)))
+        
+    elif mode in ['stmt', 'expr']:
+        if len(tree.body) != 1:
+            raise ValueError('Mode "{}" requires exactly one statement '
+                             '(got {})'.format(mode, len(tree.body)))
+        tree = tree.body[0]
+        if mode == 'expr':
+            if not isinstance(tree, Expr):
+                raise ValueError('Mode "{}" requires Expr node (got {})'
+                                 .format(mode, type(tree).__name__))
+            tree = tree.value
+    
+    elif mode is not None:
+        raise ValueError('Unknown parse mode "' + mode + '"')
+    
+    return tree
+
+
+class MacroProcessor(PatternTransformer):
+    
+    """Substitutes specific function and method calls with ASTs.
+    Works on the input tree in a bottom-up fashion.
+    
+    Four patterns are supported: function expressions (fe), function
+    statements (fs), method expressions (me), and method statements
+    (ms). The fe and fs patterns match a function call where the
+    function is syntactically specified by a Name node. The me and ms
+    patterns apply to method calls. All of these patterns require that
+    the call have no fancy arguments (i.e. no keywords, varargs, etc.).
+    
+    Handlers are defined as methods in subclasses, similar to visit_*
+    methods in NodeVisitor subclasses. The names have form
+    
+        handle_KIND_NAME
+    
+    where KIND is one of the four pattern types ("fe", etc.) and NAME
+    is the syntactic function or method name to match.
+    
+    Handlers take in two arguments: the function name and a tuple of
+    the syntactic argument ASTs. Method handlers are similar, but the
+    AST of the receiver (i.e. self) object is prepended as the first
+    AST in the argument tuple. Handlers return the replacement tree,
+    or None to skip the current match.
+    """
+    
+    func_expr_pattern = (
+        Call(Name(PatVar('_func'), Load()),
+             PatVar('_args'),
+             (), None, None))
+    meth_expr_pattern = (
+        Call(Attribute(PatVar('_self'), PatVar('_meth'), Load()),
+             PatVar('_args'),
+             (), None, None))
+    func_stmt_pattern = Expr(func_expr_pattern)
+    meth_stmt_pattern = Expr(meth_expr_pattern)
+    
+    def dispatch_func(self, _func, _args):
+        """Dispatch to a fe or fs handler."""
+        handler = getattr(self, 'handle_fe_' + _func, None)
+        if handler is None:
+            handler = getattr(self, 'handle_fs_' + _func, None)
+        
+        if handler is not None:
+            return handler(_func, _args)
+    
+    def dispatch_meth(self, _self, _meth, _args):
+        """Dispatch to a me or ms handler."""
+        handler = getattr(self, 'handle_me_' + _meth, None)
+        if handler is None:
+            handler = getattr(self, 'handle_ms_' + _meth, None)
+        
+        if handler is not None:
+            return handler(_meth, (_self,) + _args)
+    
+    def __init__(self):
+        patrepls = [(self.func_expr_pattern, self.dispatch_func),
+                    (self.func_stmt_pattern, self.dispatch_func),
+                    (self.meth_expr_pattern, self.dispatch_meth),
+                    (self.meth_stmt_pattern, self.dispatch_meth)]
+        super().__init__(patrepls)
---
+++ iast/test_pylang.py
@@ -0,0 +1,58 @@
+"""Unit tests for pylang.py."""
+
+
+import unittest
+
+from iast.node import parse, Name, Load, Expr, Module, Tuple, Pass, Num
+
+from iast.pylang import *
+
+
+class PylangCase(unittest.TestCase):
+    
+    def testExtract(self):
+        tree_in = parse('x')
+        
+        tree_out = extract_mod(tree_in, mode='mod')
+        self.assertEqual(tree_out, tree_in)
+        
+        tree_out = extract_mod(tree_in, mode='code')
+        exp_tree_out = (Expr(Name('x', Load())),)
+        self.assertEqual(tree_out, exp_tree_out)
+        
+        tree_out = extract_mod(Module([]), mode='stmt_or_blank')
+        self.assertEqual(tree_out, None)
+        tree_out = extract_mod(tree_in, mode='stmt_or_blank')
+        self.assertEqual(tree_out, Expr(Name('x', Load())))
+        with self.assertRaises(ValueError):
+            extract_mod(parse('x; y'), mode='stmt_or_blank')
+        
+        tree_out = extract_mod(tree_in, mode='stmt')
+        self.assertEqual(tree_out, Expr(Name('x', Load())))
+        with self.assertRaises(ValueError):
+            extract_mod(Module([]), mode='stmt')
+        with self.assertRaises(ValueError):
+            extract_mod(parse('x; y'), mode='stmt')
+        
+        tree_out = extract_mod(tree_in, mode='expr')
+        exp_tree_out = Name('x', Load())
+        self.assertEqual(tree_out, exp_tree_out)
+        with self.assertRaises(ValueError):
+            extract_mod(parse('pass'), mode='expr')
+    
+    def testMacro(self):
+        class Foo(MacroProcessor):
+            def handle_ms_foo(self, f, args):
+                if len(args) > 1:
+                    return Tuple(args, Load())
+            def handle_fe_bar(self, f, args):
+                return Num(5)
+        
+        tree = parse('o.foo(bar(1))')
+        tree = Foo.run(tree)
+        exp_tree = parse('(o, 5)')
+        self.assertEqual(tree, exp_tree)
+
+
+if __name__ == '__main__':
+    unittest.main()
