14358c26e7342a66a99605d78457ba062429a9ed
Jon Brandvein <jon.brandvein@gmail.com>
2014-12-18 01:12:01 -0500

    Added generation of AST nodes from ASDL.

--- iast/node.py
+++ iast/node.py
@@ -9,6 +9,7 @@ from simplestruct import Struct, Field, MetaStruct
 __all__ = [
     'AST',
     'dump',
+    'nodes_from_asdl',
 ]
 
 
@@ -67,3 +68,80 @@ def dump(tree, indent=0):
                 end)
     else:
         return repr(tree)
+
+
+class ASTBuilder:
+    
+    """Given an ASDL structure, return a mapping from each name of
+    an AST node to a tuple of information describing it. The tuple
+    consists of:
+    
+        1) a list of field specifications, which are triples of a
+           field name, a type name (either an ASDL primitive or
+           another node type), and a quantifier ('', '?', or '*')
+        
+        2) the name of a base node type it inherits from
+    """
+    
+    # In the style of asdl.VisitorBase.
+    
+    def run(self, mod):
+        self.info = {}
+        self.visit(mod)
+        return self.info
+    
+    def visit(self, obj, *args):
+        methname = 'visit' + obj.__class__.__name__
+        meth = getattr(self, methname)
+        return meth(obj, *args)
+    
+    def visitModule(self, mod):
+        for dfn in mod.dfns:
+            self.visit(dfn)
+    
+    def visitType(self, type):
+        self.visit(type.value, str(type.name))
+    
+    def visitSum(self, sum, name):
+        for t in sum.types:
+            self.visit(t, name)
+        self.info[name] = ([], 'AST')
+    
+    def visitConstructor(self, cons, name):
+        fields = []
+        for f in cons.fields:
+            fields.append(self.visit(f, cons.name))
+        self.info[cons.name] = (fields, name)
+    
+    def visitField(self, field, name):
+        assert not (field.seq and field.opt)
+        quant = '*' if field.seq else '?' if field.opt else ''
+        return (field.name, field.type, quant)
+    
+    def visitProduct(self, prod, name):
+        fields = []
+        for f in prod.fields:
+            fields.append(self.visit(f, name))
+        self.info[name] = (fields, 'AST')
+
+def nodes_from_asdl(asdl_tree, module=None):
+    """Given an ASDL structure, return a mapping from node type
+    names to node types. If module is given, it should be the
+    name of a module whose global namespace will contain the
+    returned node types. (This allows instances of the node
+    classes to be pickled.)
+    """
+    lang = {'AST': AST}
+    info = ASTBuilder().run(asdl_tree)
+    for name, (fields, _base) in info.items():
+        namespace = {'__module__': module,
+                     '_fields': tuple(fn for fn, ft, fq in fields)}
+        new_node = type(name, (AST,), namespace)
+        lang[name] = new_node
+    for name, node in lang.items():
+        if name == 'AST':
+            continue
+        _fields, base = info[name]
+        basecls = lang[base]
+        node.__bases__ = (basecls,)
+    return lang
--- tests/test_node.py
+++ tests/test_node.py
@@ -6,6 +6,7 @@ import unittest
 from simplestruct import Field
 
 from iast.util import trim
+from iast.asdl import parse as asdl_parse
 from iast.node import *
 
 
@@ -32,7 +33,7 @@ class NodeCase(unittest.TestCase):
             _fields = ['operands']
         tree = Sum((Add(1, 2), Sum((3, 4,)), Sum((5,)), Sum(()),))
         s = dump(tree)
-        exp_s = trim('''\
+        exp_s = trim('''
             Sum(operands = (Add(left = 1,
                                 right = 2),
                             Sum(operands = (3,
@@ -45,6 +46,31 @@ class NodeCase(unittest.TestCase):
         # Reconstruct the tree from dump.
         tree2 = eval(s, locals())
         self.assertEqual(tree2, tree)
+    
+    def test_from_asdl(self):
+        spec = trim('''
+            module Dummy
+            {
+                expr = Add(expr left, expr right)
+                     | Sum(expr* operands)
+                     | Num(num val)
+                num = (int abs, object sign)
+            }
+            ''')
+        asdl = asdl_parse(spec)
+        lang = nodes_from_asdl(asdl)
+        
+        self.assertEqual(lang['AST'], AST)
+        self.assertEqual(lang['expr']._fields, ())
+        self.assertEqual(lang['expr'].__bases__, (lang['AST'],))
+        self.assertEqual(lang['Add']._fields, ('left', 'right'))
+        self.assertEqual(lang['Add'].__bases__, (lang['expr'],))
+        self.assertEqual(lang['Sum']._fields, ('operands',))
+        self.assertEqual(lang['Sum'].__bases__, (lang['expr'],))
+        self.assertEqual(lang['Num']._fields, ('val',))
+        self.assertEqual(lang['Num'].__bases__, (lang['expr'],))
+        self.assertEqual(lang['num']._fields, ('abs', 'sign'))
+        self.assertEqual(lang['num'].__bases__, (lang['AST'],))
 
 
 if __name__ == '__main__':
